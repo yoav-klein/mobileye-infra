83497fcddce31c65a786b882372e3579f92a51a9
Yoav Klein <yoavklein25@gmail.com>
2022-11-01 13:58:54 +0200

    Adding force_delete to ECR

--- application/ecs/main.tf
+++ application/ecs/main.tf
@@ -8,6 +8,7 @@ locals {
 resource "aws_ecr_repository" "ecr_repository" {
   name                 = local.container_name
   image_tag_mutability = "MUTABLE"
+  force_delete = true
 
   image_scanning_configuration {
     scan_on_push = false
