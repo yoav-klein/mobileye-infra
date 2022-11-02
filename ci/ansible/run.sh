2ba94f73228ded21a666720b69c0819d3a36773d
Yoav Klein <yoavklein25@gmail.com>
2022-11-02 12:57:58 +0200

    Ansible code to install Jenkins

--- ci/ansible/run.sh
+++ ci/ansible/run.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-#docker run -v $PWD:/ansible -w /ansible yoavklein3/ansible:latest ansible-galaxy collection list
+if [ -z "$AWS_ACCESS_KEY" ] || [ -z "$AWS_SECRET_KEY" ]; then
+    echo "Set the AWS_ACCESS_KEY and AWS_SECRET_KEY environment variables"
+    exit 1
+fi
 
-#docker run -v $PWD:/ansible -w /ansible \
-#    yoavklein3/ansible:latest ansible-inventory -i aws_ec2.yaml --graph
-
-
-docker run -v $PWD:/ansible -w /ansible -e ANSIBLE_HOST_KEY_CHECKING=false yoavklein3/ansible:latest \
+docker run -v $PWD:/ansible -w /ansible \
+    -e ANSIBLE_HOST_KEY_CHECKING=false \
+    -e AWS_ACCESS_KEY="$AWS_ACCESS_KEY" \
+    -e AWS_SECRET_KEY="$AWS_SECRET_KEY" \
+     yoavklein3/ansible:latest \
      ansible-playbook --key-file key -i aws_ec2.yaml jenkins-playbook.yaml
 
-#docker run -v $PWD:/ansible yoavklein3/ansible:latest ansible-config --version
