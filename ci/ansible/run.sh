e780739532a85e995abc1de637ceb7f345125785
Yoav Klein <yoavklein25@gmail.com>
2022-11-03 15:37:51 +0200

    Ansible code for CI

--- ci/ansible/run.sh
+++ ci/ansible/run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if [ -z "$AWS_ACCESS_KEY" ] || [ -z "$AWS_SECRET_KEY" ]; then
+if [ -z "$AWS_ACCESS_KEY_ID" ] || [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
     echo "Set the AWS_ACCESS_KEY and AWS_SECRET_KEY environment variables"
     exit 1
 fi
 
 docker run -v $PWD:/ansible -w /ansible \
     -e ANSIBLE_HOST_KEY_CHECKING=false \
-    -e AWS_ACCESS_KEY="$AWS_ACCESS_KEY" \
-    -e AWS_SECRET_KEY="$AWS_SECRET_KEY" \
+    -e AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" \
+    -e AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" \
      yoavklein3/ansible:latest \
      ansible-playbook --key-file key -i aws_ec2.yaml jenkins-playbook.yaml
 
